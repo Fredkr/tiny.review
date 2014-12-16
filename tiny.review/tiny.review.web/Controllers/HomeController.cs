f66029bd69dde88121aa59173d42f85e74183121
Fredrik Källström <fredrik.kallstrom@tretton37.com>
2014-12-16 20:15:53 +0100

    MVC 5 project created, removed all unnecessary stuff

---
+++ tiny.review/tiny.review.web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace tiny.review.web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Nothing to see here, move along";
+        }
+    }
+}
---
+++ tiny.review/tiny.review.web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="tiny.review.web.MvcApplication" Language="C#" %>
