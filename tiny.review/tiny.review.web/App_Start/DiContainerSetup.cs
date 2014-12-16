f8904ba7ee569439bcec18f5a7fbb73497db6612
Fredrik Källström <fredrik.kallstrom@tretton37.com>
2014-12-16 20:32:28 +0100

    Autofac added and set up

---
+++ tiny.review/tiny.review.web/App_Start/DiContainerSetup.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using System.Linq;
+using System.Reflection;
+using System.Web.Compilation;
+using System.Web.Mvc;
+
+namespace tiny.review.web.App_Start
+{
+    internal class DiContainerSetup
+    {
+        public void BuildContainer()
+        {
+            var builder = new ContainerBuilder();
+            var referencedAssemblies = BuildManager.GetReferencedAssemblies().Cast<Assembly>().ToArray();
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterAssemblyModules(referencedAssemblies);
+
+            var container = builder.Build();
+            var resolver = new AutofacDependencyResolver(container);
+            DependencyResolver.SetResolver(resolver);
+        }
+    }
+}
