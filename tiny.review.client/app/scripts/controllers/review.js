3e8307684395960116d96e3047528a01fcfef91f
Fredkr <fredrik.kallstrom@tretton37.com>
2014-12-29 15:52:00 +0100

    Initial table-list structure for reviews created

--- tiny.review.client/app/scripts/controllers/review.js
+++ tiny.review.client/app/scripts/controllers/review.js
@@ -3,13 +3,16 @@ angular.module('tinyreviewclientApp')
   .controller('ReviewCtrl', function ($scope, GetReviewService, LoginService) {
   	$scope.user = LoginService.getLoginState().user;
   	$scope.reviews = [];
+		$scope.getRating = function(num) {
+		    return new Array(num);   
+		};
 
   	(function() {
      	var reviews = GetReviewService.getReviews($scope.user.Name);
    		reviews.then(function(result) {  
-   			if(!!result.data && result.data.Success){
+   			if(!!result.data){
    				$scope.reviews = result.data;
         }
-      	});      
+      });      
     })();
   });
