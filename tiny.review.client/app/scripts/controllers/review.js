e1f7c512412957ecf567ab4a39e3d3386b2d5e9d
Fredkr <fredrik.kallstrom@tretton37.com>
2015-01-08 17:27:27 +0100

    Added paging to the review view, using ui.bootstrap

--- tiny.review.client/app/scripts/controllers/review.js
+++ tiny.review.client/app/scripts/controllers/review.js
@@ -2,16 +2,32 @@
 angular.module('tinyreviewclientApp')
   .controller('ReviewCtrl', function ($scope, GetReviewService, LoginService) {
   	$scope.user = LoginService.getLoginState().user;
-  	$scope.reviews = [];
+    $scope.currentPage = 1;
+    $scope.numPerPage = 6;
+    $scope.filteredReviews = [];
+    $scope.reviews = [];
+
 		$scope.getRating = function(num) {
 		    return new Array(num);   
 		};
+
+    $scope.changePage = function() {
+      var begin = (($scope.currentPage - 1) * $scope.numPerPage);
+      var end = begin + $scope.numPerPage;      
+      $scope.filteredReviews = $scope.reviews.slice(begin, end);
+    };
+
+    $scope.$watch('currentPage + numPerPage', function() {
+      $scope.changePage();
+    });
 
   	(function() {
      	var reviews = GetReviewService.getReviews($scope.user.Name);
    		reviews.then(function(result) {  
    			if(!!result.data){
    				$scope.reviews = result.data;
+          $scope.totalReviews = result.data.length;
+          $scope.changePage();
         }
       });      
     })();
